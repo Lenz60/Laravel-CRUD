45c9784c2232f2a657f37460c5d390f9f6154f53
Rafly Andrian <raflyandrian@gmail.com>
2023-10-30 22:42:53 +0700

    Added Laravel Breeze

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="flex flex-col border-2 border-yellow-500">
+            <div>
+                <p>
+                    The significance of such problems is so obvious that social
+                    economic development represents an interesting experiment of
+                    economic practicability of decisions made. Likewise new
+                    model of the organisational activity plays an important part
+                    in the formation of the step-by-step consistent development
+                    of the society. Likewise new model of the organisational
+                    activity requires the clarification of further ways of
+                    development.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
